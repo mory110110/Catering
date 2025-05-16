e1150e6cca6f137a1f9198c32bb63681726415af
Morteza Shariatee <97549487+mory110110@users.noreply.github.com>
2025-05-16 16:24:40 -0500

    Responsive design of navbar with hamburgur menu for phones

---
+++ js/myFunction.js
@@ -0,0 +1,18 @@
+/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+function myFunction() {
+  var x = document.getElementById("main-nav");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+} 
+
+function navToggle() { // creates a new function named navToggle
+  var x = document.getElementById("main-nav"); // creates a variable set to #main-nav
+    if (x.className === "topnav") { // if #main-nav has a class of .topnav,
+    x.className += " responsive";   // then add a new class
+  } else {                          // if it doesn't,
+    x.className = "topnav";         // set class to .topnav
+  } //closes if-else
+} //closes function
